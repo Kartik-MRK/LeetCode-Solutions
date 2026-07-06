3058bd4c21a38b66963e5d65f8a697bab865bc4f
KARTIK MAVINKATTA <mavinkattakartik43@gmail.com>
2026-07-06 12:41:45 +0530

    [1929] [Time Beats: 18.35%] [Memory Beats: 9.94%] - Leet2Hub

--- 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.py
+++ 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.py
@@ -1,1 +1,10 @@
-Guys, please don't get demotivate if you don't solve these questions, be patience, do practice and be consistent.
+1class Solution:
+2    def getConcatenation(self, nums: List[int]) -> List[int]:
+3        n=len(nums)
+4        ans=[]
+5        for i in range (2*n):
+6            ans.append(nums[i%n])
+7
+8        return ans
+9
+10        
