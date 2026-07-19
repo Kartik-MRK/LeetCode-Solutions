796c2161d4c75f0e79133a6ede01c780aa31b259
KARTIK MAVINKATTA <mavinkattakartik43@gmail.com>
2026-07-19 11:38:28 +0530

    [746] [Time Beats: 19.95%] [Memory Beats: 14.68%] - Leet2Hub

--- 01-Arrays-and-Hashing/746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.py
+++ 01-Arrays-and-Hashing/746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.py
@@ -1,11 +1,21 @@
 1class Solution:
 2    def minCostClimbingStairs(self, cost: List[int]) -> int:
-3        mincost=float(inf)
-4        n=len(cost)
-5        for i in range(n-3,-1,-1):
-6            cost[i]+=min(cost[i+1],cost[i+2])
-7        return min(cost[0],cost[1])
-8
-9        
-10
-11        
+3        n=len(cost)
+4        memo=[-1 for i in range(n+1)]
+5        def dfs(step):
+6            if step>=n+1:
+7                return 0
+8            if step>=n-2:
+9                return cost[step]
+10            if memo[step]!=-1:
+11                return memo[step]
+12            
+13            memo[step]=min(dfs(step+1),dfs(step+2))+cost[step]
+14            return memo[step]
+15
+16        return min(dfs(0),dfs(1))
+17
+18
+19        
+20
+21        
