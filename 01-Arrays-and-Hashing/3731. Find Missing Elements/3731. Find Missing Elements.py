af7925914bdb95e4faf3d3509300f0b6dee1816f
KARTIK MAVINKATTA <mavinkattakartik43@gmail.com>
2026-08-04 17:57:06 +0530

    [3731] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/3731. Find Missing Elements/3731. Find Missing Elements.py
@@ -0,0 +1,1 @@
+class Solution:    def findMissingElements(self, nums: List[int]) -> List[int]:        nums.sort()        res=[]        for i in range(len(nums)-1):            if nums[i]!=nums[i+1]+1:                for i in range(nums[i]+1,nums[i+1]):                    res.append(i)        return res        
